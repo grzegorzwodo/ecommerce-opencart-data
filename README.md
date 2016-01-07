b062dc46b78606242ba9cfffc5e61cbafa107667
Grzegorz <grzegorz.wodo@seamless.se>
2016-01-07 11:49:09 +0100

    init opencart data

---
+++ README.md
@@ -0,0 +1,27 @@
+### ecommerce-opencart-data
+Docker data container for opencart
+
+#### How to run this
+
+```bash  
+
+
+```
+
+##### Mounting /var/www/html to the local machine
+
+```bash
+
+
+```
+
+##### Mounting only plugin directory to the local machine
+
+```bash
+
+
+```
+
+
+#### Directory permissions
+
---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ -z "$PS_VERSION" ]; then
+  echo "Prestashop version not found. Please set up OC_VERSION variable properly."
+  exit 1
+fi
+
+curl -s -o /tmp/temp "https://github.com/opencart/opencart/archive/$OC_VERSION.zip"
+unzip -q /tmp/temp -d /tmp
+cp -rf /tmp/prestashop/* /var/www/html
+rm -rf /tmp/*
+chown -R www-data:www-data /var/www
+
+if [ -n "$PLUGIN_NAME" ]; then
+  chmod -Rf 777 /var/www/html/modules/$PLUGIN_NAME
+fi
+
+if [ -n "$1" ]; then
+	exec $1
+elseif
+	exec /bin/true
+fi
