34667c3b6ba5e752388d572a05b68c814b19a2e1
Grzegorz <grzegorz.wodo@seamless.se>
2016-01-15 15:27:03 +0100

    Add config files

--- run.sh
+++ run.sh
@@ -11,6 +11,14 @@ unzip -q /tmp/$OC_VERSION.zip -d /tmp
 cp -rf /tmp/opencart-$OC_VERSION/* /var/www/html
 rm -rf /tmp/*
 chown -R www-data:www-data /var/www
+cp /var/www/html/upload/config-dist.php /var/www/html/upload/config.php
+cp /var/www/html/upload/admin/config-dist.php /var/www/html/upload/admin/config.php
+
+chown www-data /var/www/html/upload/config.php
+chgrp www-data /var/www/html/upload/config.php
+
+chown www-data /var/www/html/upload/admin/config.php
+chgrp www-data /var/www/html/upload/admin/config.php
 
 #if [ -n "$PLUGIN_NAME" ]; then
 #  chmod -Rf 777 /var/www/html/modules/$PLUGIN_NAME
