395e1afddfe2252d5ff278271591ecb5eb9c7485
Grzegorz <grzegorz.wodo@seamless.se>
2016-01-18 14:35:56 +0100

    Add seqr plugin files

--- run.sh
+++ run.sh
@@ -26,9 +26,35 @@ chgrp www-data /var/www/html/upload/admin/config.php
 
 #copy plugin files
 cp /var/www/html/plugin/seqr/admin/controller/payment/seqr.php /var/www/html/upload/admin/controller/payment/seqr.php
+chown www-data /var/www/html/upload/admin/controller/payment/seqr.ph
+chgrp www-data /var/www/html/upload/admin/controller/payment/seqr.ph
+
+cp /var/www/html/plugin/seqr/admin/language/english/payment/seqr.php /var/www/html/upload/admin/language/english/payment/seqr.php
+chown www-data /var/www/html/upload/admin/language/english/payment/seqr.php
+chgrp www-data /var/www/html/upload/admin/language/english/payment/seqr.php
+
+cp /var/www/html/plugin/seqr/admin/view/template/payment/seqr.tpl /var/www/html/upload/admin/view/template/payment/seqr.tpl
+chown www-data /var/www/html/upload/admin/view/template/payment/seqr.tpl
+chgrp www-data /var/www/html/upload/admin/view/template/payment/seqr.tpl
+
+cp /var/www/html/plugin/seqr/catalog/controller/payment/seqr.php /var/www/html/upload/catalog/controller/payment/seqr.php
+chown www-data /var/www/html/upload/catalog/controller/payment/seqr.php
+chgrp www-data /var/www/html/upload/catalog/controller/payment/seqr.php
+
+cp /var/www/html/plugin/seqr/catalog/language/english/payment/seqr.php /var/www/html/upload/catalog/language/english/payment/seqr.php
+chown www-data /var/www/html/upload/catalog/language/english/payment/seqr.php
+chgrp www-data /var/www/html/upload/catalog/language/english/payment/seqr.php
+
+cp /var/www/html/plugin/seqr/catalog/model/payment/seqr* /var/www/html/upload/catalog/model/payment/
+chown www-data /var/www/html/upload/catalog/model/payment/seqr*
+chgrp www-data /var/www/html/upload/catalog/model/payment/seqr*
+
+cp /var/www/html/plugin/seqr/catalog/view/theme/default/template/payment/seqr.tpl /var/www/html/upload/catalog/view/theme/default/template/payment/seqr.tpl
+chown www-data /var/www/html/upload/catalog/view/theme/default/template/payment/seqr.tpl
+chgrp www-data /var/www/html/upload/catalog/view/theme/default/template/payment/seqr.tpl
 
 if [ -n "$1" ]; then
-	exec $1
+	exec $1catalog
 elseif
 	exec /bin/true
 fi
